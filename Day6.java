e42ae34c4ac74f320e6f555c171c6694bcbdbc03
Sadissxz <89706963+SadisOfc@users.noreply.github.com>
2025-05-12 18:19:24 -0500

    Día 6

---
+++ Day6.java
@@ -0,0 +1,17 @@
+public class Day6 {
+    public static void main(String[] args) {
+        int n;
+        int nnnn = 123;
+        double n2;
+        String st = "En el cielo ya solo quedaban las ascuas";
+        String numerso = "1823923";
+        n = Integer.parseInt(numerso);
+        System.out.println(n);
+        n2 = Double.parseDouble(numerso);
+        System.out.println(n2);
+        st = Integer.toString(nnnn);
+        System.out.println(st);
+        st = String.valueOf(n);
+        System.out.println(st);
+    }
+}
