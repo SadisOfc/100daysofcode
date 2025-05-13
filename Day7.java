8e4f8e855b9cd6e623ceecbccb0a272a42a35e57
Sadissxz <89706963+SadisOfc@users.noreply.github.com>
2025-05-12 19:15:10 -0500

    Día 7

---
+++ Day7.java
@@ -0,0 +1,16 @@
+public class Day7 {
+    public static void main(String[] args) {
+        int num1 = 8;
+        int num2 = 19;
+        int raizcuadrada = 16;
+        double si = 14.8;
+        double valorabsoluto = -18.3;
+
+        System.out.println(Math.max(num1, num2));
+        System.out.println(Math.min(num1, num2));
+        System.out.println(Math.sqrt(raizcuadrada));
+        System.out.println(Math.ceil(si));
+        System.out.println(Math.floor(si));
+        System.out.println(Math.abs(valorabsoluto));
+    }
+}
